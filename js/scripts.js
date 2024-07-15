fe11c0b9cd4add7aac8fac3811322599eecc9fc4
Riccardo-Mauri <riccardo90mauri@hotmail.it>
2024-07-15 15:58:35 +0200

    Fizz,Buzz e fizzbuzz in console presenti.

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,21 @@
 //3.al posto dei multipli di 5 deve stampare BUZZ
 //4.mentre per i multipli in comune del 3 e del 5 deve stamapre FIZZBUZZ
 
-for ( let i = 1; i<=100; i++){
-    console.log(i)
-}
+for (let i = 1; i <= 100; i++) {
+    //-------SE E' UN MULTIPLO DI ENTRAMBI SCRIVO FIZZBUZZ-----
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log('si è un multiplo di ENTRAMBI 3 e 5, quindi FIZZBUZZ');
+    }
+    //-------VERIFICO SE E' UN MULTIPLO DI 3 E SCRIVO FIZZ-------
+    else{
+        if (i % 3 == 0){
+            console.log('si, è un multiplo di 3 quindi FIZZ');
+        }
+        else if (i % 5 == 0){
+            console.log('si, è un multiplo di 5 quindi BUZZ');
+        }
+        else{
+            console.log('qui niente, quindi inserisco numero normale:' + i);
+        }
+    }
+}   
