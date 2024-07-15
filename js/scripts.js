9c58739b630ecb69a5c9881cbcfa223508bf6e3b
Riccardo-Mauri <riccardo90mauri@hotmail.it>
2024-07-15 15:19:08 +0200

    inizio esercizio

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+//1. fare un tabelal automatica che parta da 1 fino ad arrivare a 100
+//2.al posto dei multipli di 3 deve stamapre FIZZ
+//3.al posto dei multipli di 5 deve stampare BUZZ
+//4.mentre per i multipli in comune del 3 e del 5 deve stamapre FIZZBUZZ
+
+for ( let i = 1; i<=100; i++){
+    console.log(i)
+}
